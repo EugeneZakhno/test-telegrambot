66417027808967a0918136667f01c999eaa8e9e0
EugeneZakhno <zohan1234567@gmail.com>
2020-10-27 23:37:28 +0300

    Init

---
+++ src/main/java/ru/intervale/testtelegrambot/TestTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package ru.intervale.testtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTelegrambotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/intervale/testtelegrambot/TestTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intervale.testtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTelegrambotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
