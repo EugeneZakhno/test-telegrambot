0bf9db4dc4e152f52ce446d2c47c51b93b511e8d
EugeneZakhno <zohan1234567@gmail.com>
2020-10-28 22:37:01 +0300

    added MyTestTelegramBot and BotConfig

---
+++ src/main/java/ru/intervale/testtelegrambot/appconfig/BotConfig.java
@@ -0,0 +1,41 @@
+package ru.intervale.testtelegrambot.appconfig;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+import ru.intervale.testtelegrambot.MyTestTelegramBot;
+
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public MyTestTelegramBot MySuperTelegramBot() {
+        DefaultBotOptions options = ApiContext
+                .getInstance(DefaultBotOptions.class);
+
+        options.setProxyHost(proxyHost);
+        options.setProxyPort(proxyPort);
+        options.setProxyType(proxyType);
+
+        MyTestTelegramBot mySuperTelegramBot = new MyTestTelegramBot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
+}
---
+++ src/main/java/ru/intervale/testtelegrambot/controllers/WebHookController.java
@@ -0,0 +1,26 @@
+package ru.intervale.testtelegrambot.controllers;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.intervale.testtelegrambot.MyTestTelegramBot;
+
+
+@RestController
+public class WebHookController {
+    private final MyTestTelegramBot telegramBot;
+
+    public WebHookController(MyTestTelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+
+}
